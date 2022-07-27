0e54f55fd1c4c14efb6af542065ab8156fb7b8d9
Takuya Toyokawa <takuya.k.toyokawa@protonmail.com>
2022-07-26 18:04:11 -0700

    Delete function & sort by time
    - need to refactor code tilll
    - add animations
    - flesh out design
    - up UX / UI for logging in and signing up
    - fix header state

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,13 @@
-export default function ListItem(){
+export default function ListItem({data, handleDelete}){
     return (
-        <li className="flex p-4 font-bold rounded-lg border-2 border-gray-200">
-            hello
+        <li className="flex justify-between p-4 font-bold rounded-lg border-2 border-gray-200">
+            <p>{data.note}</p>
+            <p onClick={handleDelete}>delete</p>
         </li>
     )
+}
+
+ListItem.defaultProps = {
+    text: "default",
+    handleDelete: () => {}
 }
--- src/routes/TodoList.js
+++ src/routes/TodoList.js
@@ -1,20 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ListItem from "../components/ListItem";
 import TextField from "../components/TextField";
 import { PrimaryButton } from "../components/Buttons";
 import { uploadNote } from '../modules/HandleUserPost';
+import { database } from '../firebase.config';
+import { deleteNote } from '../modules/HandleUserPost';
+import { onSnapshot, collection, deleteDoc, doc } from "firebase/firestore";
+
 
 export default function TodoList(uid) {
   const [note, setNote] = useState()
+  const [allNotes, setAllNotes] = useState([])
+
+  useEffect(()=>{
+    if(uid.uid){
+      onSnapshot(collection(database, "users", uid.uid, "notes"), (data) => {
+        const notesData = data.docs.map(item => item.data()).sort((a,b)=> a.created.seconds < b.created.seconds)
+        setAllNotes(notesData)
+      })
+    }
+  }, [])
+  
+  // Delete collection
   return (
     <div className="flex flex-col gap-8">
-      <form className="flex w-full p-0 gap-4">
+      <form onSubmit={(e)=>e.preventDefault()}className="flex w-full p-0 gap-4">
         <TextField handleOnChange={(e)=>setNote(e.target.value)} placeholder="Add note here..." />
         <PrimaryButton handleOnClick={ () => uploadNote(uid.uid, note) }>Add</PrimaryButton>
       </form>
       <hr />
       <ul className="flex flex-col gap-4">
-        <ListItem />
+        {allNotes.map(note=>
+          <ListItem handleDelete={()=>deleteNote(uid.uid, note.id)} data={note}/>
+        )}
       </ul>
     </div>
   );
