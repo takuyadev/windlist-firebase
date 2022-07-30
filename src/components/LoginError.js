8c75062df53dd69b7cfe036b7ff2244d8e59484e
Takuya Toyokawa <takuya.k.toyokawa@protonmail.com>
2022-07-30 00:00:31 -0700

    update to error page

--- src/components/LoginError.js
+++ src/components/LoginError.js
@@ -1,7 +1,14 @@
-export function LoginError(){
-    return (
-        <div>
-            Please log in to your account to see your page.
-        </div>
-    )
+import { Link } from "react-router-dom";
+
+export function LoginError() {
+  return (
+    <div className="text-center">
+        <h1 className="text-2xl font-bold mb-4">Error</h1>
+      Please{" "}
+      <span className="text-red-500 hover:text-red-300 duration-100">
+        <Link to="/Login">log in</Link>
+      </span>{" "}
+      to your account to see your page.
+    </div>
+  );
 }
